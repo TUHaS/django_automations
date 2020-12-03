3327559bff37e540147403a070ac6527fc40e1ee
tuhas <rileydeq@gmail.com>
2020-12-03 22:07:27 +0300

    add sitetree dump

--- automations_app/views.py
+++ automations_app/views.py
@@ -29,16 +29,23 @@ class TestersView(SingleTableView, views.APIView):
     permission_classes = [permissions.IsAdminUser]
 
     def get(self, request, *args, **kwargs):
+        # получение всех записей из модели (таблицы) Hour
         objs_hours = Hour.objects.all()
         array = np.zeros((len(objs_hours), 2), dtype=np.float)
+        # формирование выборки (сложность проекта + зарплата тестера)
         for idx, obj in enumerate(objs_hours):
             array[idx, 0] = obj.application.complication
             array[idx, 1] = float(obj.tester.salary)
+        # вычисление центров для масс и получение меток для выборки
         labels = KMeans(n_clusters=3, random_state=0).fit_predict(array)
+        # преобразование изображения в base64
         img = self.plot_to_base64(array, labels)
+        # формирование объекта таблицы (django-tables2)
         table = self.table_class(User.objects.all())
+        # настройка пагинатора таблицы (ограничение в 10 записей на страницу)
         RequestConfig(request, paginate={"per_page": 10}).configure(table)
         context = {"image": img, 'table': table}
+        # возвращение ответа на запрос (рендеринг HTML шаблона и передача в него изображения
         return render(request, self.template_name, context)
 
     @staticmethod
@@ -51,22 +58,25 @@ class TestersView(SingleTableView, views.APIView):
         return image_data
 
 
-class TestedApplicationsView(SingleTableView):
+class TestedApplicationsView(SingleTableView, views.APIView):
     model = TestedApplication
     table_class = TestedApplicationsTable
     template_name = "automations_app/table.html"
+    permission_classes = [permissions.IsAuthenticated]
 
 
-class ToolsView(SingleTableView):
+class ToolsView(SingleTableView, views.APIView):
     model = Tool
     table_class = ToolsTable
     template_name = "automations_app/table.html"
+    permission_classes = [permissions.IsAuthenticated]
 
 
-class HoursView(SingleTableView):
+class HoursView(SingleTableView, views.APIView):
     model = Hour
     table_class = HoursTable
     template_name = "automations_app/table.html"
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class RegistrationView(FormView):
@@ -78,6 +88,8 @@ class RegistrationView(FormView):
         form.save()
         username = self.request.POST['username']
         password = self.request.POST['password1']
+        # заходим под пользователем после регистрации,
+        # чтобы он не проходил аутентификацию снова
         user = authenticate(self.request, username=username, password=password)
         login(self.request, user)
         return super().form_valid(form)
